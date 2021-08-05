20a81fded9e75e606616144877cf51c68eeeeae4
Mohan Jemini <mohan.jemini@sparity.com>
2021-08-05 21:17:43 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import pkg from "../package.json";
+import queries from "./queries/index.js";
+import schemas from "./schemas/index.js";
+
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {ReactionAPI} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "ProductSearch",
+    name: "productsearch",
+    version: pkg.version,
+    collections: {
+      Products: {
+        name: "Products",
+        indexes: [
+          // Create indexes. We set specific names for backwards compatibility
+          // with indexes created by the aldeed:schema-index Meteor package.
+          [{ ancestors: 1 }, { name: "c2_ancestors" }],
+          [{ createdAt: 1 }, { name: "c2_createdAt" }],
+          [{ handle: 1 }, { name: "c2_handle" }],
+          [{ hashtags: 1 }, { name: "c2_hashtags" }],
+          [{ shopId: 1 }, { name: "c2_shopId" }],
+          [{ "workflow.status": 1 }, { name: "c2_workflow.status" }],
+          // Use _id as second sort to force full stability
+          [{ updatedAt: 1, _id: 1 }]
+        ]
+      }
+    },
+    graphQL: {
+      schemas
+    },
+    queries
+  });
+}
